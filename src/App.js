79c9e3e1b3e98708c01a6a1954f099d9f0046999
Biren000 <birenbhalodiya19@gmail.com>
2023-03-14 12:22:46 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import Cruud from "./components/crud2/Cruud";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Search from "./components/searching/Search";
 import Sort from "./components/sorting/Sort";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <Todo /> */}
-      {/* <Crud /> */}
-      <Cruud />
-      {/* <Search /> */}
-      {/* <Sort /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Cruud />} />
+          <Route path="/crud" element={<Crud />} />
+          {/* <Route path="/todo" element={<Todo />} /> */}
+          <Route path="/search" element={<Search />} />
+          <Route path="/sort" element={<Sort />} />
+        </Routes>
+      </Router>
     </>
   );
 }
